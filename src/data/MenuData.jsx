4d1f4421d1e44f570f9358390bfbc65f5547566a
Brandon_Hart <sugb73@gmail.com>
2022-11-24 01:41:42 -0500

    Added menu items

---
+++ src/data/MenuData.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function MenuData() {
+  const data = [
+    {
+      name: 'Tuna',
+      price: '15',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis tempora veritatis veniam eum dolor molestiae eligendi ab.',
+    },
+    {
+      name: 'Lobster Mac and Cheese',
+      price: '20',
+      description:
+        'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi, maiores.',
+    },
+    {
+      name: 'Shrimp Cocktail',
+      price: '15',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ratione sequi at sit doloribus magni eveniet itaque.',
+    },
+    {
+      name: 'Bruschetta',
+      price: '15',
+      description:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis iure modi quaerat aspernatur at?',
+    },
+    {
+      name: '',
+      price: '',
+      description: '',
+    },
+    {
+      name: '',
+      price: '',
+      description: '',
+    },
+    {
+      name: '',
+      price: '',
+      description: '',
+    },
+    {
+      name: '',
+      price: '',
+      description: '',
+    },
+  ];
+}
